0417393f2963c4b387edebedc8d9af91872a8187
Pilar Leminski Veiga <pilar.leminski.veiga@avanade.com>
2022-09-15 17:36:17 -0300

    Aula 4 - estruturas de repetição e condicionais

--- dist/main.js
+++ dist/main.js
@@ -1,32 +1,73 @@
 "use strict";
-const nameUser = 'Pilar Veiga'; //identificador
-const idade = 38;
-const statusUser = 1;
-if (statusUser == 1) {
+/*
+const nameUser:string = 'Pilar Veiga'; //identificador
+const idade:number = 38;
+const statusUser:number = 1;
+
+if (statusUser == 1){
     console.log("Usuário");
-}
-else if (statusUser == 2) {
+}else if (statusUser == 2){
     console.log("Empresa");
-}
-else if (statusUser == 3) {
+}else if (statusUser == 3){
     console.log("Administrador");
-}
-else {
+}else {
     console.log("Inválido");
 }
-if (idade <= 2) {
+
+switch(statusUser){
+    case 1:
+        console.log("Usuário");
+        break;
+    case 2:
+        console.log("Empresa");
+        break;
+    case 3:
+    case 4:
+        console.log("Administrador");
+        break;
+    default:
+        console.log("Inválido")
+}
+
+if(idade <= 2){
     console.log("Bebê");
-}
-else if (idade <= 11) {
+}else if (idade <=11){
     console.log("Criança");
-}
-else if (idade <= 19) {
+}else if (idade <=19){
     console.log("Adolescente");
-}
-else if (idade <= 59) {
+}else if (idade <=59) {
     console.log("Adulto");
-}
-else {
+}else {
     console.log("Idoso");
 }
 console.log(`Seja bem vindo ao curso de TypeScript ${nameUser}`);
+
+// estrutura de repetição
+const yearFactory = new Date().getFullYear();
+for(let x = 1920; x <= yearFactory; x++){
+    console.log(x);
+}
+
+console.log("---------------------------------------");
+for(let x = yearFactory; x >= 1920; x--){
+    console.log(x);
+}
+
+console.log("---------------------------------------");
+for(let x = yearFactory; x >= 1920; x = x - 10){
+    if(x >= 1990 && x < 2000){
+        continue;
+    }
+    console.log(x);
+}
+*/
+console.log("TABUADA");
+for (let x = 0; x <= 10; x++) {
+    if (x > 2 && x < 10) {
+        if (x % 2 == 1 && x > 3) {
+            console.log(".");
+        }
+        continue;
+    }
+    console.log("2 x " + x + " = " + 2 * x);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/*
 const nameUser:string = 'Pilar Veiga'; //identificador
 const idade:number = 38;
 const statusUser:number = 1;
@@ -12,6 +13,21 @@ if (statusUser == 1){
     console.log("Inválido");
 }
 
+switch(statusUser){
+    case 1:
+        console.log("Usuário");
+        break;
+    case 2:
+        console.log("Empresa");
+        break;
+    case 3:
+    case 4:
+        console.log("Administrador");
+        break;
+    default:
+        console.log("Inválido")
+}
+
 if(idade <= 2){
     console.log("Bebê");
 }else if (idade <=11){
@@ -24,3 +40,33 @@ if(idade <= 2){
     console.log("Idoso");
 }
 console.log(`Seja bem vindo ao curso de TypeScript ${nameUser}`);
+
+// estrutura de repetição
+const yearFactory = new Date().getFullYear();
+for(let x = 1920; x <= yearFactory; x++){
+    console.log(x);
+}
+
+console.log("---------------------------------------");
+for(let x = yearFactory; x >= 1920; x--){
+    console.log(x);
+}
+
+console.log("---------------------------------------");
+for(let x = yearFactory; x >= 1920; x = x - 10){
+    if(x >= 1990 && x < 2000){
+        continue;
+    }
+    console.log(x);
+}
+*/
+console.log("TABUADA")
+for(let x = 0; x <= 10; x++){
+    if(x > 2 && x < 10){
+        if(x % 2 == 1 && x > 3){
+            console.log(".")
+        }
+        continue;
+    }
+    console.log("2 x "+x+" = "+2*x);
+}
