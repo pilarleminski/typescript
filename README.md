b7f55d555f74a33c9e303137e72a6028e8f75e50
Pilar Leminski Veiga <pilar.leminski.veiga@avanade.com>
2022-09-22 13:58:53 -0300

    "Getters e setters"

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 tsc -w para iniciar o typescritp
 nodemon ./dist/arquivo onde irá rodar o nodemon
+
+// encapsulamento - modificadores de acesso.
+    //public - acessível por todos
+    //protected - acessível apenas dentro da classe e das classes filhas
+    //private - acessível apenas dentro da classe
+    //static - Não precisa instanciar a classe para acessar o método ou propriedade
+
+
--- src/Funcionario/Funcionario.ts
+++ src/Funcionario/Funcionario.ts
@@ -1,19 +1,49 @@
 import Pessoa from "../Pessoa";
 
-class Funcionario extends Pessoa{
+class Funcionario extends Pessoa{ //Pessoa é a classe base e Funcionário é a classe extendida
     ri:string;
     cpf:string;
+    static ipAddress: string = '127.0.0.1';
+    private _salario: number = 0;
+    isAdmin:boolean = true;
 
-    constructor(parametroNome: string, parametroIdade: number, parametroRi: string, parametroCpf: string){
+    constructor(parametroNome: string, parametroIdade: number, parametroRi: string, parametroCpf: string, parametroSalario: number){
         super(parametroNome, parametroIdade);
         this.ri = parametroRi;
         this.cpf = parametroCpf;
-        this.cadFuncionario();
+        this.salario = parametroSalario;
+        console.log(`Funcionário ${this.nome} de idade ${this.idade} ganha ${this.salario}`);
     }
-    cadFuncionario(){
-        console.log(`Funcionário ${this.nome} cadastrado com sucesso!`);
+
+//Sempre que tivermos uma propriedade privada criamos o getter e o setter
+//Getter - controla como você vê a propriedade
+
+    get salario(): number {
+        return (this.isAdmin) ? this._salario : 0;
+    }
+//Setter - controla como você manipula ela
+    set salario(valor:number){
+        if(valor < 1200) {
+            this._salario = 1200;
+        }else{
+            this._salario = valor;
+        }
     }
 }
 
 export default Funcionario
-const hcoder = new Funcionario("Paula", 23, "1342545", "123.123.123-00");
+// criando um objeto que instancia ou representa a classe Funcionario
+const hcoder = new Funcionario("Paula", 23, "1342545", "123.123.123-00", 1000);
+console.log("Chamando a propriedade nome usando o objeto: NOME-", hcoder.nome);
+console.log("Acessando a propriedade estática: " + Funcionario.ipAddress);
+
+/* isAdmin = false
+Funcionário Paula de idade 23 ganha 0
+Chamando a propriedade nome usando o objeto: NOME- Paula
+Acessando a propriedade estática: 127.0.0.1
+*/
+/* isAdmin = true
+Funcionário Paula de idade 23 ganha 1200
+Chamando a propriedade nome usando o objeto: NOME- Paula
+Acessando a propriedade estática: 127.0.0.1
+*/
